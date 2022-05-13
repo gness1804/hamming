5ef545051b0431eb045188bdc0111bde142dac3e
gness1804 <graham.nessler@gmail.com>
2022-05-13 09:52:28 -0500

    all tests passing

--- hamming.ts
+++ hamming.ts
@@ -1,6 +1,7 @@
 export function compute(left: string, right: string): number {
   let diffs = 0;
-  left.split(',').forEach((letter, index) => {
+  if (left.length !== right.length) throw new Error('DNA strands must be of equal length.');
+  left.split('').forEach((letter, index) => {
     if (letter !== right[index]) diffs++;
   })
   return diffs;
